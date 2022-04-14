af24389335c4b484aa91fe08b55f9b1726001301
Maks <mikastricyn@outlook.com>
2022-04-14 12:20:26 +0300

    Added ExpApproximate and swap a, b with move from line to PowApproximate

--- lab4/OrdinaryLeastSquares/ExpApproximate.py
+++ lab4/OrdinaryLeastSquares/ExpApproximate.py
@@ -4,7 +4,7 @@ from basic import OrdinaryLeastSquareBasicApproximation
 from lineApproximation import LineApproximate
 
 
-class PowApproximate(OrdinaryLeastSquareBasicApproximation):
+class ExpApproximate(OrdinaryLeastSquareBasicApproximation):
 
     def __init__(self, points: list[tuple[float, float]]):
         super().__init__(points)
@@ -12,14 +12,14 @@ class PowApproximate(OrdinaryLeastSquareBasicApproximation):
         self.a_ = None
 
     def approximate(self):
-        if not all(point[0] > 0 and point[1] > 0 for point in self.points):
+        if not all(point[1] > 0 for point in self.points):
             return
 
-        lin_points = [(log(self.x[i]), log(self.y[i])) for i in range(self.n)]
+        lin_points = [(self.x[i], log(self.y[i])) for i in range(self.n)]
         line_approximate = LineApproximate(lin_points)
 
-        a = self.a_ = exp(line_approximate.ba_)
+        a = self.a_ = exp(line_approximate.b_)
         b = self.b_ = line_approximate.a_
 
-        self.func = lambda x: a * (x ** b)
-        self.func_str = f"{round(a, 3)}*x^{round(b, 3)}"
+        self.func = lambda x: a * exp(x * b)
+        self.func_str = f"{round(a, 3)} * e^({round(b, 3)}x)"
