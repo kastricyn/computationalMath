1f2778cb49f1d801e0b2f2da2d9b2cd9724736e0
Maks <mikastricyn@outlook.com>
2022-04-14 11:39:21 +0300

    Added approximation for LineApproximate

--- lab4/OrdinaryLeastSquares/basic.py
+++ lab4/OrdinaryLeastSquares/basic.py
@@ -1,8 +1,33 @@
+from metrics import Metrix
+
+
 class OrdinaryLeastSquareBasicApproximation:
     def __init__(self, points: list[tuple[float, float]]):
         self.points = points
         self.x = (point[0] for point in points)
         self.y = (point[1] for point in points)
         self.n = len(points)
+        self.func = None
+        self.func_str: str | None = None
+        self.resultY: list[float] | None = None
+        self.metrix: dict[str, float] | None = None
+
+        self.approximate()
+        self.updateResultY()
+        self.count_metrix()
+
+    def approximate(self):
+        '''
+        Must be overridden for certain method
+        :return: None,
+        but change self.func, self.func_str
+        '''
+        pass
+
+    def count_metrix(self):
+        # todo: self.metrix['']
+        pass
 
 
+    def updateResultY(self):
+        self.resultY = [self.func(i) for i in self.x]
