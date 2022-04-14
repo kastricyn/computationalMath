3e439ab655a6d5a016524e00b2d4d17044761796
Maks <mikastricyn@outlook.com>
2022-04-14 12:08:15 +0300

    Added PowApproximate

--- lab4/OrdinaryLeastSquares/lineApproximation.py
+++ lab4/OrdinaryLeastSquares/lineApproximation.py
@@ -3,6 +3,11 @@ from basic import OrdinaryLeastSquareBasicApproximation
 
 class LineApproximate(OrdinaryLeastSquareBasicApproximation):
 
+    def __init__(self, points: list[tuple[float, float]]):
+        super().__init__(points)
+        self.a_ = None
+        self.b_ = None
+
     def approximate(self):
         sx = sum(self.x)
         sxx = sum(x ** 2 for x in self.x)
@@ -13,8 +18,8 @@ class LineApproximate(OrdinaryLeastSquareBasicApproximation):
         d1 = sxy * self.n * sx * sy
         d2 = sxx * sy - sx * sxy
 
-        a = d1 / d
-        b = d2 / d
+        self.a_ = d1 / d
+        self.b_ = d2 / d
 
-        self.func = lambda x: a * x + b
-        self.func_str = f"{round(a, 3)} * x + {round(b, 3)}"
+        self.func = lambda x: self.a_ * x + self.b_
+        self.func_str = f"{round(self.a_, 3)} * x + {round(self.b_, 3)}"
